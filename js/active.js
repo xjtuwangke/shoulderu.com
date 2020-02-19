50cdc4c9121e44d77ac4982e7b455b8c722b5b76
kevin <kwang@rollong.com>
2020-02-19 20:16:19 +0800

    initial commit

---
+++ js/active.js
@@ -0,0 +1,28 @@
+﻿$(function(){
+    window.onresize=function(){
+        setfontsize();
+    }
+    function setfontsize()
+    {
+        if(document.documentElement.clientWidth>1920)
+        {
+            document.documentElement.style.fontSize = document.documentElement.clientWidth / 80 + 'px';
+        }
+        else
+        {
+            document.documentElement.style.fontSize = '';
+        }
+    }
+    setfontsize();
+    $(".menu .menubtn").click(function(){
+        $(".menu").addClass("active")
+    })
+    $(".menubg,.menuclose").click(function(){
+        $(".menu").removeClass("active")
+    })
+    $(".tutor_box ul li").hover(function(){
+        $(this).addClass("active");
+    },function(){
+        $(this).removeClass("active");
+    });
+})
